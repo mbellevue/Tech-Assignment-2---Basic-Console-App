d587c78d8207403dfa3b4e8356196825799516f5
Mardochee Bellevue <mbellevue@usf.edu>
2022-01-28 19:34:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Program developed by: Mardochee Bellevue
+
+using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Mardochee Bellevue.");
+            Console.WriteLine("I love to eat, read, and watch movies.");
+            Console.WriteLine("I also enjoy learning new technologies.");
+            Console.WriteLine("I am majoring in Business Analytics and Information Systems.");
+        }
+    }
+}
